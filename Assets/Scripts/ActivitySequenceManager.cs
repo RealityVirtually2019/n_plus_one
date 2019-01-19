50c260053cb2ff0883f922bcaa5144fe5fa76bbb
E P <undergroundlab001@gmail.com>
2019-01-19 00:54:19 -0500

    added basic IActivity and Sequence Manager

---
+++ Assets/Scripts/ActivitySequenceManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivitySequenceManager : MonoBehaviour {
+    public IActivity[] activities;
+
+	// Use this for initialization
+	void Start () {
+        activities = new IActivity[]
+        {
+
+        };
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+
