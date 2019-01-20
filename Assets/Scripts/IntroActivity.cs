b93a342bef0b096f7bb920b0db911a479b716f61
E P <undergroundlab001@gmail.com>
2019-01-20 09:15:05 -0500

    image target working, made skeleton for intro activity

---
+++ Assets/Scripts/IntroActivity.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.MagicLeap;
+
+
+public class IntroActivity : IActivity
+{
+    public GameObject prefab;
+    public ActivitySequenceManager activitySequenceManager;
+    private GameObject introObj;
+
+    public IntroActivity(ActivitySequenceManager asm)
+    {
+        activitySequenceManager = asm;
+    }
+
+    public void play()
+    {
+        introObj = GameObject.Instantiate(Resources.Load("IntroObj")) as GameObject;
+        introObj.GetComponent<IntroObjScr>().activity = this;
+        throw new System.NotImplementedException();
+    }
+
+    public void taskCompleted()
+    {
+        GameObject.Destroy(introObj);
+        activitySequenceManager.goToNextActivity();
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/IntroObjScr.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroObjScr : MonoBehaviour {
+    public IntroActivity activity;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator waitAndThenFinish()
+    {
+        yield return new WaitForSeconds(2);
+        activity.taskCompleted();
+    }
+}
